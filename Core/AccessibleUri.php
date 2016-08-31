2dd6f0cf0a8ae50a9d666cb97523ef270011b234
Dominik Klapuch <klapuchdominik@gmail.com>
2016-08-31 20:42:02 +0200

    Added badge and reformated code

--- Core/AccessibleUri.php
+++ Core/AccessibleUri.php
@@ -6,6 +6,7 @@ namespace Klapuch\Uri;
  * URI which is always reachable
  */
 final class AccessibleUri implements Uri {
+    const NOT_FOUND = 404;
     private $uri;
 
     public function __construct(string $uri) {
@@ -14,7 +15,7 @@ final class AccessibleUri implements Uri {
 
     public function reference(): string {
         $headers = @get_headers($this->uri);
-        if($headers && strpos($headers[0], '404') === false)
+        if($headers && strpos($headers[0], self::NOT_FOUND) === false)
             return $this->uri;
         throw new \InvalidArgumentException(
             sprintf(
--- Core/Uri.php
+++ Core/Uri.php
@@ -3,10 +3,10 @@ declare(strict_types = 1);
 namespace Klapuch\Uri;
 
 interface Uri {
-	/**
+    /**
      * Address or name of the resource pointed by URI
-	 * @throws \InvalidArgumentException
-	 * @return string
-	 */
-	public function reference(): string;
+     * @throws \InvalidArgumentException
+     * @return string
+     */
+    public function reference(): string;
 }
